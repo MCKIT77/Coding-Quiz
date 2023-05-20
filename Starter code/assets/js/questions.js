7c90e3fad8c66a23041b4d219613852bb2624499
Jacob McKitrick <jakemckit@gmail.com>
2023-05-20 17:42:18 -0400

    added starter code and a few additions to the questions.js

---
+++ Starter code/assets/js/questions.js
@@ -0,0 +1,28 @@
+// list of all questions, choices, and answers
+var questions = [
+  {
+    title: 'What does HTML stand for?',
+    choices: ['Hyper Text-to Make Language', 'Hyper Text Mark-up Language', 'Hyper Typed Make-up Language', 'Hyper Text Marker Language'],
+    answer: 'Hyper Text Mark-up Language',
+  },
+  {
+    title: 'What does CSS stand for?',
+    choices: ['Central Style Sheets', 'Choice Style Sheets', 'Chosen Style Selectors', 'Cascading Style Sheets'],
+    answer: 'Cascading Style Sheets',
+  },
+  {
+    title: 'When desired type is a string, what symbol is to be placed before and after the desired content?',
+    choices: ['Quotations', 'Brackets', 'Parenthesis', 'Semi-colons'],
+    answer: 'Quotations',
+  },
+  {
+    title: 'To iterate over each item in an array, what method would be best suited for the task?',
+    choices: ['If Statement', 'Infinite Loop', 'For Loop', 'Conditional Statement'],
+    answer: 'For Loop',
+  },
+  {
+    title: 'To change an object to JSON notation, what method would be used?',
+    choices: ['JSON.parse(object)', 'JSON.stringify(object)', 'JSON.changeObj(object)', 'JSON.toJSON(object)'],
+    answer: 'JSON.stringify(object)'
+  }
+];
